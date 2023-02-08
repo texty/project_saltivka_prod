11f549e7bb3334c82795b3201e0b182675d41232
HubashovD <34233868+HubashovD@users.noreply.github.com>
2023-02-08 13:07:53 +0200

    update with mapboxgl

--- js/map.js
+++ js/map.js
@@ -1,50 +1,79 @@
-var map = L.map('map', {
-    scrollWheelZoom: false,
-    zoomControl: false
-}).setView([46.70950052897342, 32.41241455078126], 12);
+const bounds = [
+    [36.333, 50.031], // Southwest coordinates
+    [36.3790, 50.046] // Northeast coordinates
+];
 
-new L.Control.Zoom({ position: 'topright' }).addTo(map);
-
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}'
-"https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+//Якщо в тебе є мапбокс-токен свій, то додай
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXZnZXNoYWRyb3pkb3ZhIiwiYSI6ImNsZGQyY2w5dDBjb2gzcG8xeXQ3c2EzczEifQ.lYkv8Hg7kFKNdNJF7wx4mg';
 
 
+const map = new mapboxgl.Map({
+    container: 'map',
+    // в мене є гіпотеза, що якщо не ставити мапбоксівські тайли, то він не рахує завантаження...
+    style: "./data/osm_liberty.json",
+    center: [36.37, 50.03],
+    zoom: 14,
+    pitch: 100,
+    bearing: 160,
+    antialias: true,
+    maxBounds: bounds
+});
 
-
-L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
-    maxZoom: 17,
-    minZoom: 4,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
-
-// https://raw.githubusercontent.com/HubashovD/project_mykolaiv_shelling_web/main/tiles/tileset/{z}/{x}/{y}.png
-
-
-
-// var southWest = L.latLng(51.60172, 32.08003),
-//     northEast = L.latLng(51.01369, 33.00698);
-// var bounds = L.latLngBounds(southWest, northEast);
-
-// map.setMaxBounds(bounds);
-// map.on('drag', function() {
-//     map.panInsideBounds(bounds, { animate: true });
-// });
-
-var frontLineStyles = {
-    "color": "#f3a6b2",
-    "fillColor": "none",
-    "stroke-width": 10
-};
-
-fetch("../data/buildings.geojson")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        geoData = L.geoJSON(data, {
-            style: frontLineStyles,
-        })
-        geoData.setStyle({ 'className': 'buildings' })
-        geoData.addTo(map);
+map.on('load', () => {
+    map.addSource('floorplan', {
+        'type': 'geojson',
+        'data': './data/buildings_2.geojson'
     });
+    map.addLayer({
+        'id': 'room-extrusion',
+        'type': 'fill-extrusion',
+        'source': 'floorplan',
+        'paint': {
+            'fill-extrusion-color': "#d0d0d0",
+            'fill-extrusion-height': ['get', 'level_heigt'],
+            // 'fill-extrusion-base': ['get', 'base_height'],
+            'fill-extrusion-opacity': 1
+        }
+    });
+
+    map.addSource('highlighted', {
+        'type': 'geojson',
+        'data': './data/damaged_building_2.geojson'
+    });
+    map.addLayer({
+        'id': 'red',
+        'type': 'fill-extrusion',
+        'source': 'highlighted',
+        'paint': {
+            'fill-extrusion-color': "#d3999a",
+            'fill-extrusion-height': ['get', 'level_heigt'],
+            // 'fill-extrusion-base': ['get', 'base_height'],
+            'fill-extrusion-opacity': 1
+        }
+    });
+});
+
+map.on("click", 'red', function(e) {
+    console.log(e.features[0].properties)
+
+    d3.select('#infoboard').html(
+        // 'id ' + userData.properties[0] + '<br/>' +
+        '<span class="info-head">Адреса:</span><br> <span class="info-info">' + e.features[0].properties['name'] + '</span><br>' +
+        '<span class="info-head">Категорія:</span><br> <span class="info-info">' + e.features[0].properties['опис'] + '</span><br>' +
+        '<span class="info-head">Опис:</span><br> <span class="info-info">' + e.features[0].properties['як пошкоджено'] + '</span><br>' +
+        '<video class="vid-info"width="100%" autoplay loop muted poster="./photo/' + e.features[0].properties['фото'] + '"><source src="./video_s/' + e.features[0].properties['відео'] +
+        '" type="video/mp4"></video>' + '<br>'
+        // 'photo ' + userData.properties[23] + '<br>'
+        // 'video ' + userData.properties[18] + '<br>'
+    )
+
+    //ось тут логіка, що має бути по кліку
+})
+
+map.on("mouseenter", 'red', function(e) {
+    map.getCanvas().style.cursor = "pointer";
+});
+
+map.on('mouseleave', 'red', () => {
+    map.getCanvas().style.cursor = ''
+})
